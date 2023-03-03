7b1a872bc838c4875063314e74745d3ec4c94b99
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-03 06:15:17 -0500

    Added the scoreboard class

---
+++ BugSquashLib/Scoreboard.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file Scoreboard.cpp
+ * @author Ricar
+ */
+
+#include "Scoreboard.h"
---
+++ BugSquashLib/Scoreboard.h
@@ -0,0 +1,22 @@
+/**
+ * @file Scoreboard.h
+ * @author Ricar
+ *
+ *
+ */
+
+#ifndef PROJECT1_BUGSQUASHLIB_SCOREBOARD_H
+#define PROJECT1_BUGSQUASHLIB_SCOREBOARD_H
+
+class Scoreboard
+{
+private:
+	int mOops;
+	int mMissed;
+	int mFixed;
+
+public:
+
+};
+
+#endif //PROJECT1_BUGSQUASHLIB_SCOREBOARD_H
