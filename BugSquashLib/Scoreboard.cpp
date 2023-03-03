a005620b075d1709443a3b3ead275eabc6f0cd62
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-03 07:43:49 -0500

    Added the constructor and functions for Scoreboard

--- BugSquashLib/Scoreboard.cpp
+++ BugSquashLib/Scoreboard.cpp
@@ -4,3 +4,21 @@
  */
 
 #include "Scoreboard.h"
+#include "BugSquash.h"
+#include "Item.h"
+
+Scoreboard::Scoreboard(BugSquash *bugsquash) : mBugSquash(bugsquash)
+{
+}
+
+void Scoreboard::SquashIncrement(Item* item){
+
+}
+
+void Scoreboard::ProgramIncrement(){}
+
+void Scoreboard::Reset(){
+	mFixed = 0;
+	mMissed = 0;
+	mOops = 0;
+}
--- BugSquashLib/Scoreboard.h
+++ BugSquashLib/Scoreboard.h
@@ -1,21 +1,30 @@
 /**
  * @file Scoreboard.h
- * @author Ricar
+ * @author Ricardo Flores
  *
  *
  */
 
 #ifndef PROJECT1_BUGSQUASHLIB_SCOREBOARD_H
 #define PROJECT1_BUGSQUASHLIB_SCOREBOARD_H
+#include "pch.h"
+#include "BugSquash.h"
+class Item;
 
 class Scoreboard
 {
 private:
-	int mOops;
-	int mMissed;
-	int mFixed;
+	BugSquash* mBugSquash = nullptr;
+	int mFixed = 0;
+	int mMissed = 0;
+	int mOops = 0;
 
 public:
+	Scoreboard(BugSquash* bugsqaush);
+
+	void SquashIncrement(Item* item);
+	void ProgramIncrement();
+	void Reset();
 
 };
 
