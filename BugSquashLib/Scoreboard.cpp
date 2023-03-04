a376fc7bab174b3f5517de6b59083034f0de305b
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-03 19:22:36 -0500

    Deleted old commented out code and copy and pasted constant on top of .cpp

--- BugSquashLib/Scoreboard.cpp
+++ BugSquashLib/Scoreboard.cpp
@@ -7,15 +7,28 @@
 #include "BugSquash.h"
 #include "Item.h"
 
+/// Score font size to use
+const int ScoreSize = 85;
+
+/// Lable for score font size to use
+const int LabelSize = 40;
+
+/// The font color to use
+const wxColour FontColor = wxColour(0, 200, 200);
+
+/// Left score X location. The right score is
+/// the width minus this value.
+const int LeftScoreX = 150;
+
+/// Score Y location
+const int ScoreY = 20;
+
+/// Score label Y location
+const int ScoreLabelY = 100;
+
 Scoreboard::Scoreboard(BugSquash *bugsquash) : mBugSquash(bugsquash)
 {
 }
-
-//void Scoreboard::SquashIncrement(){
-//	///if item is a feature it will increment the oops count
-//	///if item is not a feature (normal bug) then increment the fixed count
-//}
-
 
 void Scoreboard::FixedIncrement()
 {
