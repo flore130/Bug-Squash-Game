6ad89368bbe62b9d62e05b7793c4122935535e2a
marsh532 <marsh532@msu.edu>
2023-03-07 16:43:02 -0500

    Added constants to Null/Redundancy bug classes

--- BugSquashLib/BugNull.cpp
+++ BugSquashLib/BugNull.cpp
@@ -9,14 +9,23 @@
 
 using namespace std;
 
-/// Bug filename
-const wstring BugNullImageName = L"images/scarlet-gray-bug.png";
+/// The bug sprite image
+const wstring NullBugSpriteImageName = L"scarlet-gray-bug.png";
+
+/// The splat image
+const wstring NullBugSplatImageName = L"scarlet-gray-splat.png";
+
+/// Number of sprite images
+const int NullBugNumSpriteImages = 6;
+
+
+
 
 /**
  * Constructor
  * @param level Level that this bug is a member of
  */
 
-BugNull::BugNull(Level *level) : Bug(level, BugNullImageName)
+BugNull::BugNull(Level *level) : Bug(level, NullBugSpriteImageName)
 {
 }
