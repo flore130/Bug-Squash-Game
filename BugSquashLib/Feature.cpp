567aa9dd0d34fbeba074443217e30347bd6b7161
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-08 13:16:34 -0500

    Made the Feature Class

---
+++ BugSquashLib/Feature.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Feature.cpp
+ * @author Ricar
+ */
+
+#include "Feature.h"
+
+/// The feature sprite image
+const std::wstring FeatureImageName = L"feature.png";
+
+/// The splat image
+const std::wstring FeatureSplatImageName = L"feature-splat.png";
+
+/// Number of sprite images
+const int FeatureNumSpriteImages = 7;
+
+Feature::Feature(Level* level) : Bug(level, FeatureImageName)
+{
+
+}
