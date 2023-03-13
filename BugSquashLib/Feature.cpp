08a13badf9552b6e45b273f729c2734f50fe4ae7
marsh532 <marsh532@msu.edu>
2023-03-13 19:23:21 -0400

    Update BugSquashLib/Feature.cpp

--- BugSquashLib/Feature.cpp
+++ BugSquashLib/Feature.cpp
@@ -8,10 +8,10 @@
 #include "Feature.h"
 
 /// The feature sprite image
-const std::wstring FeatureImageName = L"feature.png";
+const std::wstring FeatureImageName = L"images/feature.png";
 
 /// The splat image
-const std::wstring FeatureSplatImageName = L"feature-splat.png";
+const std::wstring FeatureSplatImageName = L"images/feature-splat.png";
 
 /// Number of sprite images
 const int FeatureNumSpriteImages = 7;
@@ -20,3 +20,28 @@ Feature::Feature(Level* level) : Bug(level, FeatureImageName)
 {
 
 }
+
+/**
+ * Save this bug to an XML node
+ * @param node The parent node we are going to be a child of
+ * @return
+ */
+wxXmlNode* Feature::XmlSave(wxXmlNode* node)
+{
+	auto itemNode = Item::XmlSave(node);
+
+	itemNode->AddAttribute(L"type", L"feature");
+
+
+	return itemNode;
+}
+
+/**
+ * Load this XML Node to bug
+ * @param node The parent node we are going to be a child of
+ */
+void Feature::XmlLoad(wxXmlNode* node)
+{
+	Item::XmlLoad(node);
+}
+
