5362e9c4f7810f6eb2f5174a06c20c1337db4e9f
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-15 13:41:29 -0400

    Add the functions necessary to show scoreboard on the screen

--- BugSquashLib/Scoreboard.cpp
+++ BugSquashLib/Scoreboard.cpp
@@ -4,9 +4,16 @@
  */
 
 #include "pch.h"
+#include <wx/wx.h>
+#include <wx/graphics.h>
+
+
 #include "Scoreboard.h"
 #include "BugSquash.h"
 #include "Item.h"
+#include "string"
+
+using namespace std;
 
 /// Score font size to use
 const int ScoreSize = 85;
@@ -31,7 +38,7 @@ const int ScoreLabelY = 100;
  * Constructor
  * @param bugsquash a pointer to a bugsquash object
  */
-Scoreboard::Scoreboard(BugSquash *bugsquash) : mBugSquash(bugsquash)
+Scoreboard::Scoreboard(BugSquash* bugsquash) : mBugSquash(bugsquash)
 {
 }
 
@@ -67,4 +74,35 @@ void Scoreboard::Reset(){
 	mFixed = 0;
 	mMissed = 0;
 	mOops = 0;
+}
+
+void Scoreboard::Draw(std::shared_ptr<wxGraphicsContext> graphics)
+{
+	DrawScore(graphics, L"Fixed", LeftScoreX, mFixed);
+	DrawScore(graphics, L"Missed", BugSquash::Width/2, mMissed);
+	DrawScore(graphics, L"Oops", BugSquash::Width - LeftScoreX, mOops);
+}
+
+//string
+//upcalls
+//width and height
+//constructor for bugsqussh call
+
+void Scoreboard::DrawScore(std::shared_ptr<wxGraphicsContext> graphics, std::wstring label, float x, int score)
+{
+	wxFont scoreFont(wxSize(1,ScoreSize), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	wxFont labelFont(wxSize(1, LabelSize), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	graphics->SetFont(scoreFont, FontColor);
+	double width = 1;
+	double height = 1;
+	std::string s = std::to_string(score);
+
+	graphics->GetTextExtent(s, &width, &height);
+	graphics->DrawText(s, x-width/2, ScoreY);
+
+	graphics->SetFont(labelFont, FontColor);
+
+	graphics->GetTextExtent(label, &width, &height);
+	graphics->DrawText(label, x-width/2, ScoreLabelY);
+
 }
