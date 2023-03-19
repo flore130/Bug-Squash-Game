3def6614dc2f1912cc7d8abee8c3402b48e6f4fc
Kyle Nowak <nowakky1@msu.edu>
2023-03-19 16:30:40 -0400

    Add FatBugVisitor class

---
+++ BugSquashLib/FatBugSquashVisitor.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file FatBugSquashVisitor.cpp
+ * @author Kyle Nowak
+ */
+
+#include "pch.h"
+#include "FatBugSquashVisitor.h"
+
---
+++ BugSquashLib/FatBugSquashVisitor.h
@@ -0,0 +1,21 @@
+/**
+ * @file FatBugSquashVisitor.h
+ * @author Kyle Nowak
+ *
+ * Concrete visitor used to squash Fat Bugs
+ */
+
+#ifndef PROJECT1_BUGSQUASHLIB_FATBUGSQUASHVISITOR_H
+#define PROJECT1_BUGSQUASHLIB_FATBUGSQUASHVISITOR_H
+
+#include "ItemVisitor.h"
+
+class FatBugSquashVisitor : public ItemVisitor
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_BUGSQUASHLIB_FATBUGSQUASHVISITOR_H
