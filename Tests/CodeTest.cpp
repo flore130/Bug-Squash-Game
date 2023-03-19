71184548bd54f99487da0914f1e0201fd03436d4
Kyle Nowak <nowakky1@msu.edu>
2023-03-19 10:03:51 -0400

    Update Code tests

--- Tests/CodeTest.cpp
+++ Tests/CodeTest.cpp
@@ -13,36 +13,58 @@ using namespace std;
 class CodeTest : public ::testing::Test {
 protected:
 	shared_ptr<Code> mCode = nullptr;
+
+	/**
+	 * Load the proper xml file into the code block
+	 * @param filename the name of the xml file
+	 */
+	void Load(const wstring &filename)
+	{
+		wxXmlDocument doc;
+		ASSERT_TRUE(doc.Load(filename));
+
+		auto node = doc.GetRoot();
+		mCode = make_shared<Code>(node);
+
+		cout << "Initial code: \n" << mCode->GetCurrentCode() << endl;
+	}
+
+	/**
+	 * Checks if the currently loaded code would pass
+	 * if user put in correct regular expression
+	 */
+	void CheckAnswers()
+	{
+		mCode->SetCode(L"This is the wrong answer");
+
+		ASSERT_FALSE(mCode->Passes());
+
+		ASSERT_FALSE(mCode->GetExampleAnswer().empty()) <<
+														L"Code didn't have example answer provided";
+		mCode->SetCode(mCode->GetExampleAnswer());
+
+		cout << "Answer code:\n" << mCode->GetCurrentCode() << endl << endl;
+
+		ASSERT_TRUE(mCode->Passes());
+	}
 };
 
 
 TEST_F(CodeTest, Construct){
-	wxXmlDocument doc;
-	ASSERT_TRUE(doc.Load(L"../Tests/test-data/garbage-bug-1.xml"));
-
-	auto node = doc.GetRoot();
-	mCode = make_shared<Code>(node);
+	Load(L"../Tests/test-data/garbage-bug-1.xml");
 }
 
 
-TEST_F(CodeTest, Test1){
-	wxXmlDocument doc;
-	ASSERT_TRUE(doc.Load(L"../Tests/test-data/garbage-bug-1.xml"));
+TEST_F(CodeTest, AnswerChecks){
+	Load(L"../Tests/test-data/garbage-bug-1.xml");
+	ASSERT_FALSE(mCode->Passes()) << L"Garbage bug 1";
+	CheckAnswers();
 
-	auto node = doc.GetRoot();
-	mCode = make_shared<Code>(node);
+	Load(L"../Tests/test-data/garbage-bug-2.xml");
+	ASSERT_FALSE(mCode->Passes()) << L"Garbage bug 2";
+	CheckAnswers();
 
-	cout << "Initial code: \n" << mCode->GetCurrentCode() << endl;
-
-	ASSERT_FALSE(mCode->Passes());
-
-	mCode->SetCode(L"This is the wrong answer");
-
-	ASSERT_FALSE(mCode->Passes());
-
-	mCode->SetCode(mCode->GetExampleAnswer());
-
-	cout << "Answer code:\n" << mCode->GetCurrentCode() << endl;
-
-	ASSERT_TRUE(mCode->Passes());
+	Load(L"../Tests/test-data/null-bug-1.xml");
+	ASSERT_FALSE(mCode->Passes()) << L"Null bug 1";
+	CheckAnswers();
 }
