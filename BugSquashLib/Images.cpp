1efc7cd4ea44fda60477be1c89f4455facc58d0e
KevinCawley <cawkeyke@msu.edu>
2023-03-22 17:04:25 -0400

    Start on loading images only once

---
+++ BugSquashLib/Images.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file Images.cpp
+ * @author Kev
+ */
+
+#include "pch.h
+#include "Images.h"
---
+++ BugSquashLib/Images.h
@@ -0,0 +1,24 @@
+/**
+ * @file Images.h
+ * @author Kev
+ *
+ * Class to store the Images for all of the items that our program uses
+ */
+
+#ifndef PROJECT1_BUGSQUASHLIB_IMAGES_H
+#define PROJECT1_BUGSQUASHLIB_IMAGES_H
+
+
+
+/*
+ * Image class to store all of the item's images
+ */
+class Images
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_BUGSQUASHLIB_IMAGES_H
