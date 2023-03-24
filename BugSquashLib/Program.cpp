d97714b93dd819d8ebf1d3a0edbe36dc4398e9ab
Ricardo Flores <ricardofloreszaragoza@gmail.com>
2023-03-24 03:59:39 -0400

    Added constants for text properties. Also added draw functionalities. Made sure to add a function to loop through each program name

--- BugSquashLib/Program.cpp
+++ BugSquashLib/Program.cpp
@@ -13,6 +13,15 @@ const std::wstring ProgramImage = L"images/laptop.png";
 /// Program name font size
 const int ProgramNameFontSize = 22;
 
+/// font size to use
+const int TextSize = 200;
+
+/// Label for font size to use
+const int LabelSize = 140;
+
+/// The font color to use
+const wxColour FontColor = wxColour(255, 255, 255);
+
 /**
  * Constructor for feature
  * @param level The level that this program is a member of
@@ -30,3 +39,35 @@ void Program::XmlLoad(wxXmlNode* node)
 	Item::XmlLoad(node);
 }
 
+/**
+ * Draws in each program text for each respective program
+ * @param gc The graphs we are drawing on
+ */
+void Program::Draw(std::shared_ptr<wxGraphicsContext> gc)
+{
+	Item::Draw(gc);
+	for(auto programText : mProgramTexts)
+	{
+		DrawText(gc, programText, mX, mY);
+	}
+}
+
+/**
+ * Takes care of the specific font, color, and text properties
+ * @param graphics The graphs we are drawing on
+ * @param text program name
+ * @param x coordinate
+ * @param y coordinate
+ */
+void Program::DrawText(std::shared_ptr<wxGraphicsContext> graphics, wxString text, int x, int y)
+{
+	wxFont scoreFont(wxSize(ProgramNameFontSize,ProgramNameFontSize), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	wxFont labelFont(wxSize(ProgramNameFontSize, ProgramNameFontSize), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	graphics->SetFont(scoreFont, FontColor);
+	double width, height;
+
+	graphics->SetFont(labelFont, FontColor);
+
+	graphics->GetTextExtent(text, &width, &height);
+	graphics->DrawText(text, x-63, y-18);
+}
